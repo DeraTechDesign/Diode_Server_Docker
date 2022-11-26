7df651c6aac4541fce568fe3b7396db81b27fa8d
Ömer AKGÜL <37873266+tuhalf@users.noreply.github.com>
2022-11-26 23:32:04 +0300

    Fix Dir

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ echo "Checking for blockchain.sq3 file..."
 echo "Check that we have BLOCKCHAIN_SQ3_URL vars"
 test -n "$BLOCKCHAIN_SQ3_URL"
 if [ -n "$BLOCKCHAIN_SQ3_URL" ]; then
-    if [ ! -f /data/blockchain.sq3 ]; then
+    if [ ! -f /diode_server/data_prod/blockchain.sq3 ]; then
         echo "Downloading blockchain.sq3 from $BLOCKCHAIN_SQ3_URL"
         # Download the blockchain.sq3 file from the URL specified in the BLOCKCHAIN_SQ3_URL environment variable using axel
-        axel -n 10 -o /data/blockchain.sq3 $BLOCKCHAIN_SQ3_URL
+        axel -n 10 -o /diode_server/data_prod/blockchain.sq3 $BLOCKCHAIN_SQ3_URL
     # If the blockchain.sq3 file already exists, we will check if it is the latest version by comparing the file size.
     # If the file size smaller then the snapshot, we will download the latest version of the blockchain.sq3 file.
     else
@@ -16,11 +16,11 @@ if [ -n "$BLOCKCHAIN_SQ3_URL" ]; then
         # Get the file size of the blockchain.sq3 file from the URL specified in the BLOCKCHAIN_SQ3_URL environment variable
         BLOCKCHAIN_SQ3_URL_FILE_SIZE=$(curl -sI $BLOCKCHAIN_SQ3_URL | grep -i Content-Length | awk '{print $2}')
         # Get the file size of the blockchain.sq3 file in the container
-        BLOCKCHAIN_SQ3_FILE_SIZE=$(stat -c%s /data/blockchain.sq3)
+        BLOCKCHAIN_SQ3_FILE_SIZE=$(stat -c%s /diode_server/data_prod/blockchain.sq3)
         # If the file size is smaller, we will download the latest version of the blockchain.sq3 file
         if [ "$BLOCKCHAIN_SQ3_URL_FILE_SIZE" -gt "$BLOCKCHAIN_SQ3_FILE_SIZE" ]; then
             echo "Downloading blockchain.sq3 from $BLOCKCHAIN_SQ3_URL"
-            axel -n 10 -o /data/blockchain.sq3 $BLOCKCHAIN_SQ3_URL
+            axel -n 10 -o /diode_server/data_prod/blockchain.sq3 $BLOCKCHAIN_SQ3_URL
         fi
     fi
 fi
