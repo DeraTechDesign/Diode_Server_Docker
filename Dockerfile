0b9621bcb2b3a3cedec4c80fe6d3a2d3b485cb88
Ömer AKGÜL <37873266+tuhalf@users.noreply.github.com>
2022-11-26 16:56:23 +0300

    Initial Test

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Diode Server
+# Copyright 2021 Diode
+# Licensed under the Diode License, Version 1.1
+FROM elixir:1.11.4
+
+RUN apt-get update && apt-get install -y libboost-dev libboost-system-dev git axel curl
+
+ENV MIX_ENV=prod
+
+RUN git clone https://github.com/diodechain/diode_server.git
+
+WORKDIR /diode_server/
+
+RUN mix local.hex --force && mix local.rebar && mix deps.get && mix deps.compile
+
+RUN mix do compile, git_version
+
+EXPOSE 8545 8443 41046 443 993 1723 10000 51054
+
+CMD ["elixir", "-S", "mix", "run", "--no-halt"]
---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Before starting the server, if this is the first time, we need to download the latest version of the blockchain.sq3 file.
+# If BLOCKCHAIN_SQ3_URL is not set, we will skip this step.
+if [ -n "$BLOCKCHAIN_SQ3_URL" ]; then
+    if [ ! -f /data/blockchain.sq3 ]; then
+        echo "Downloading blockchain.sq3 from $BLOCKCHAIN_SQ3_URL"
+        # Download the blockchain.sq3 file from the URL specified in the BLOCKCHAIN_SQ3_URL environment variable using axel
+        axel -n 10 -o /data/blockchain.sq3 $BLOCKCHAIN_SQ3_URL
+    # If the blockchain.sq3 file already exists, we will check if it is the latest version by comparing the file size.
+    # If the file size smaller then the snapshot, we will download the latest version of the blockchain.sq3 file.
+    else
+        echo "Checking blockchain.sq3 file size"
+        # Get the file size of the blockchain.sq3 file from the URL specified in the BLOCKCHAIN_SQ3_URL environment variable
+        BLOCKCHAIN_SQ3_URL_FILE_SIZE=$(curl -sI $BLOCKCHAIN_SQ3_URL | grep -i Content-Length | awk '{print $2}')
+        # Get the file size of the blockchain.sq3 file in the container
+        BLOCKCHAIN_SQ3_FILE_SIZE=$(stat -c%s /data/blockchain.sq3)
+        # If the file size is smaller, we will download the latest version of the blockchain.sq3 file
+        if [ "$BLOCKCHAIN_SQ3_URL_FILE_SIZE" -gt "$BLOCKCHAIN_SQ3_FILE_SIZE" ]; then
+            echo "Downloading blockchain.sq3 from $BLOCKCHAIN_SQ3_URL"
+            axel -n 10 -o /data/blockchain.sq3 $BLOCKCHAIN_SQ3_URL
+        fi
+    fi
+fi
+# Before starting the server, we will do git fetch and pull to update the latest version of the source code.
+echo "Updating source code"
+git fetch
+git pull
+# Start the server using elixir -S mix run --no-halt
+echo "Starting server"
+elixir -S mix run --no-halt
+
